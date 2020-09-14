7be98f86593b26d5b3dba2c98c76168b9da6d3a5
ShadowBLR <name.men777@mail.ru>
2020-09-14 23:56:02 +0300

    добавлена 3 лаба по ООП

---
+++ OOTPLab3_new/OOTPLab3_new/Book.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTPLab3_new
+{
+    enum TypeOfBinding { Solid, Soft };
+   partial class Book
+    {
+        static uint numberOfBooks = 0;
+        const string NameOfOwner = "OAO BELARUSSIAN BOOKS"; 
+        public readonly int  id;
+        public  string Name { get; set; }
+        public string Author { get; set; }
+        public short YearOfPublishing { get; set; }
+        public ushort NumberOfPages { get; set; }
+        private int price;
+        public int Price 
+        { 
+            get 
+            {
+                return price;
+             } 
+            set
+            {
+                if (value < 0)
+                    while (value < 10)
+                        value = Convert.ToInt32(Console.ReadLine());
+                price = value;
+            }
+        }
+
+        TypeOfBinding binding=(TypeOfBinding)1;
+        public Book(string name = "",string author="" , short yearOfPublishing=0,int price = 0,ushort numberOfPages=0)
+        {
+            ++numberOfBooks;
+            Name = name;
+            Author = author;
+            YearOfPublishing = yearOfPublishing;
+            Price = price;
+            NumberOfPages = numberOfPages;
+            id = this.GetHashCode();
+        }
+        public Book(Book b)
+        {
+            Name = b.Name;
+            Author = b.Author;
+            YearOfPublishing = b.YearOfPublishing;
+            Price = b.Price;
+            NumberOfPages = b.NumberOfPages;
+            id = this.GetHashCode();
+        }
+        static Book()
+        {
+            Console.WriteLine("Создана первая книга");
+        }
+        private Book()
+        {
+            ++numberOfBooks;
+            Console.WriteLine("Типа будущий синглтон");
+        }
+         public void printBook()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Нзавание книги:" + Name);
+            Console.WriteLine("Автор книги:" + Author);
+            Console.WriteLine("Год издания:" + YearOfPublishing);
+            Console.WriteLine("Количество страниц:"+NumberOfPages);
+            Console.WriteLine("Цена:"+Price);
+            Console.WriteLine();
+        }
+         public static void  TypeOfClass()
+        {
+            Type tp = Type.GetType("OOTPLab3_new.Book");
+            Console.WriteLine(tp.AssemblyQualifiedName);
+           
+        }
+    }
+}
---
+++ OOTPLab3_new/OOTPLab3_new/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTPLab3_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book("The Picture of Dorian Gray", "Оскар Уайлд",2010, 200, 200);
+            Book.TypeOfClass();
+            Book b2 = new Book("Termonator","HZ" ,1960, 1000, 400);
+            Book b3 = new Book(b);
+            if (b.Equals(b2))
+            {
+                Console.WriteLine("Книги одинаковы 1 и 2");
+            }
+            else
+            {
+                Console.WriteLine("Книги 1 и 2 различны");
+            }
+            if (b.Equals(b3))
+            {
+                Console.WriteLine("Книги 1 и 3 одинаковы");
+            }
+            else
+            {
+                Console.WriteLine("Книги 1 и 3 различны");
+            }
+            Book []arrBook =new Book[3];
+            arrBook[0] = new Book(b);
+            arrBook[1] = new Book(b2);
+            arrBook[2] = new Book("three Musteketeers ","Duma", 1924, 1000, 2000);
+
+            Console.WriteLine("Введите автора:");
+            string buffer = Console.ReadLine();
+            Console.WriteLine("Фильтрация по автору:");
+            for(int i=0;i<arrBook.Length;i++)
+            {
+                if(arrBook[i].Author==buffer)
+                {
+                    arrBook[i].printBook();
+                }
+            }
+
+            Console.WriteLine("Введите год выпуска:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Фильтрация по году:");
+            for (int i = 0; i < arrBook.Length; i++)
+            {
+                if (arrBook[i].YearOfPublishing > year)
+                {
+                    arrBook[i].printBook();
+                }
+            }
+
+            var user = new { Name = "Tom", Age = 34 };
+            var student = new { Name = "Alice", Age = 21 };
+        }
+
+    }
+}
